1e3e705f7773a3c9a879a1097ee59a8dbf6d8173
Rxsanjeev <107175988+Rxsanjeev@users.noreply.github.com>
2022-09-26 13:40:31 +0530

    Create Assignment2.py

---
+++ Assignments/Team Member 3_SANJEEV A/Assignment2.py
@@ -0,0 +1,12 @@
+import random
+a=random.randint(1,190)
+b=random.randint(1,150)
+print(a)
+print(b)
+if a>80:
+    if b>80:
+        print ("hazar predected")
+    else:
+        print ("high tmp")
+else:
+    print ("all good")
